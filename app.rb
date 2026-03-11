52d1beff46c809b42c2d0f740043cfb95e295991
VidarKarman <vidar.karman@elev.ga.ntig.se>
2026-03-11 15:23:31 +0100

    innan bonestretches 11 mars26

--- app.rb
+++ app.rb
@@ -9,47 +9,50 @@ enable :session
 get('/') do
   slim(:home)
 end
-
-get('/register') do
-
-
-
-  slim(:register)
+get('/chat') do
+  slim(:chat)
 end
 
 post('/login') do
-  user = params["user"]
+  user = params["username"]
   pwd = params["pwd"]
-  db = SQLite3::Database.new("db/user.db")
-  result = db.execute("SELECT id,pwddigest FROM users WHERE user=?",user)
+  db = SQLite3::Database.new("db/users.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT id,pwddigest FROM users WHERE username=?",user)
 
   if result.empty?
-    redirect('/error')
+    redirect('/')
   end
   user_id = result.first["id"]
   pwd_digest = result.first["pwddigest"]
 
-  if BCrypt::Password.new(pwddigest) == pwd
+  if BCrypt::Password.new(pwd_digest) == pwd
     session[:user_id] = user_id
     redirect('/chat')
   else
-    redirect('/login')
+    redirect('/')
   end
 end
 
 post('/register') do
-  user = params["user"]
+  user = params["username"]
   pwd = params["pwd"]
   pwd_confirm = params["pwd_confirm"]
 
   db = SQLite3::Database.new("db/users.db")
-  result=db.execute("SELECT id FROM users WHERE user=?", user)
+  db.results_as_hash = true
+  result=db.execute("SELECT id FROM users WHERE username=?", user)
+  
 
   if result.empty?
     if pwd==pwd_confirm
       pwd_digest=BCrypt::Password.create(pwd)
-      db.execute("INSERT INTO users(user, pwd_digest) VALUES(?,?)", [user, pwd_digest])
-      redirect('/')
+      db.execute("INSERT INTO users(username, pwddigest) VALUES(?,?)", [user, pwd_digest])
+      redirect('/chat')
     else
       redirect('/')  #till standardsidan med error
+    end
+  else
+    redirect('/')
+  end
 end
