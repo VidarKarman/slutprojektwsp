3e9446ca4df17b57be37b9df9d30336fc8a5cc23
VidarKarman <vidar.karman@elev.ga.ntig.se>
2026-04-01 15:33:15 +0200

    bonestretch 4 april

--- app.rb
+++ app.rb
@@ -11,22 +11,35 @@ get('/') do
   slim(:home)
 end
 get('/chat') do
+  @chaten = session[:chat_with]
   db = SQLite3::Database.new("db/messages.db")
   db.results_as_hash = true
-  @chats = {}
-  @activechat = {}
-  @chats = db.execute("SELECT user2id FROM messages WHERE user1id=?", session[:user_id])
+  @activechat = db.execute(
+    "SELECT user1id, user2id, contents, time FROM messages WHERE user1id=? AND user2id=?",
+    [session[:user_id], @chaten]
+  )
+  @chats = db.execute("SELECT DISTINCT user2id FROM messages WHERE user1id=?", session[:user_id])
   slim(:chat)
 end
 post('/chat') do
-  chaten = params["chat"]
+  @chaten = params["chat"]
+  session[:chat_with] = params["chat"]
   db = SQLite3::Database.new("db/messages.db")
   db.results_as_hash = true
   @activechat = {}
-  @activechat = db.execute("SELECT user1id, user2id, contents, time FROM messages WHERE user1id=? AND user2id=?",[session[:user_id], chaten])  
+  @activechat = db.execute("SELECT user1id, user2id, contents, time FROM messages WHERE user1id=? AND user2id=?",[session[:user_id], @chaten])  
   redirect('/chat')
 end
+post('/typedmessage') do
+  halt(401, 'Not logged in') unless session[:user_id]
+  halt(400, 'No active chat selected') unless session[:chat_with]
 
+  message = params["message"]
+  db = SQLite3::Database.new("db/messages.db")
+  db.results_as_hash = true
+  db.execute("INSERT INTO messages(user1id, user2id, contents) VALUES(?,?,?)", [session[:user_id], session[:chat_with], message])
+  redirect('/chat')
+end
 post('/login') do
   user = params["username"]
   pwd = params["pwd"]
@@ -57,14 +70,12 @@ post('/register') do
   db.results_as_hash = true
   result=db.execute("SELECT id FROM users WHERE username=?", user)
   
-
   if result.empty?
     if pwd==pwd_confirm
       pwd_digest=BCrypt::Password.create(pwd)
       db.execute("INSERT INTO users(username, pwddigest) VALUES(?,?)", [user, pwd_digest])
-      db = SQLite3::Database.new("db/messages.db")
-      db.results_as_hash = true
-      db.execute("INSERT INTO messages(user1id, user2id, contents) VALUES(?,?,?)", [user, 1, "test"])
+      session[:user_id] = db.last_insert_row_id
+      session[:chat_with] = nil
       redirect('/chat')
     else
       redirect('/')  #till standardsidan med error
